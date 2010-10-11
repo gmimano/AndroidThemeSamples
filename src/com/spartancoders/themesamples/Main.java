c17aa73d00ad08ce7f574d059e3a1dcbd7ae108f
Pablo Llopis <pablo.llopis@spartancoders.com>
2010-10-11 18:34:34 +0200

    initial commit
    One sample theme available
    
    No mdpi or ldpi resources\!

---
+++ src/com/spartancoders/themesamples/Main.java
@@ -0,0 +1,47 @@
+package com.spartancoders.themesamples;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.spartancoders.themesamples.light.LightThemes;
+
+public class Main extends ListActivity {
+	private static final Map<String, Class> mSections;
+	static {
+		Map<String, Class> map = new HashMap<String, Class>();
+		map.put("Buttons", null);
+		map.put("Dark Themes", null);
+		map.put("Light Themes", LightThemes.class);
+		mSections = Collections.unmodifiableMap(map);
+	}
+	
+    private static final String [] SECTIONS = mSections.keySet().toArray(new String[3]);
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, SECTIONS);
+        setListAdapter(adapter);
+    }
+        
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+    	CharSequence text = ((TextView)v).getText();
+    	Class c = mSections.get(text);
+    	if (c != null) {
+        	Intent intent = new Intent(this, c);
+        	startActivity(intent);    		
+    	}
+    }
+}
---
+++ src/com/spartancoders/themesamples/light/Light1.java
@@ -0,0 +1,13 @@
+package com.spartancoders.themesamples.light;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.spartancoders.themesamples.R;
+
+public class Light1 extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.light1);
+	}
+}
